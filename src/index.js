6a0b144fe46e0ebafca6301dfd2fedb555f2a394
JacksonFrontEnd <evgenij_poposemov@mail.ru>
2021-02-06 20:05:09 +0300

    add new function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof(array)!="undefined")
+  {
+  if(array.length > 0){
+    var minimum = array[0];
+    for(var i = 0;i<array.length;i++){
+    if(array[i]<minimum) minimum=array[i];
+    } 
+    return minimum;
+  }
+}
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  var maximum;
+  if(typeof(array)!="undefined")
+  {
+  if(array.length > 0 && array!=null){
+    maximum=array[0];
+    for(var j = 0 ;j < array.length; j++){
+    if(array[j]>maximum) maximum=array[j];
+    } 
+    return maximum;
+  }
+}
+return 0;
 }
 
 exports.avg = function avg (array) {
+  var sum=0;
+  if(typeof(array)!="undefined")
+  {
+  if(array.length > 0){
+    for(var k=0;k<array.length;k++){
+      sum=sum+array[k];
+    }
+    return sum/array.length;
+  }
+}
   return 0;
 }
